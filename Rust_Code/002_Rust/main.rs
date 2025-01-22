75c5afa07752d7f5c49420d75bc5934da9a336da
Abhishek (Desktop) <1171abhishek.com>
2025-01-22 19:15:19 +0530

    video 3 added

--- Rust_Code/002_Rust/main.rs
+++ Rust_Code/002_Rust/main.rs
@@ -1,3 +1,15 @@
 fn main(){
-    println!("Hello World!");
+
+    print!("Hello Abhishek ");
+    print!("1234 ");
+    print!("23");
+
+    println!("Hello Abhishek ");
+    println!("1234 ");
+    println!("23");
+
+    println!("Hello world");
+
+    println!("Hello world");
+
 }
---
+++ Rust_Code/003_rust_comments/main.rs
@@ -0,0 +1,20 @@
+fn main(){
+    println!("Abhishek"); // first name
+    println!("Kumar");    // second name
+
+    // this is single line comment
+
+    /* djkfhgfu 
+    fughdfjg h
+    djfhg */
+
+    /*  riutkyu
+    fgkjhkfgjkh */
+
+    /* kuhfgh fjkhfgjhg  fdjhgfjgf  */
+
+    println!("Abhishek"); // this code pritns Abhishek
+
+    // println!("Abhishek123");
+
+}
