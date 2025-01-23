e837eabce3193ced2b6bd3dd7f658ca477521722
Abhishek (Desktop) <1171abhishek.com>
2025-01-23 19:43:21 +0530

    video 4 added

---
+++ Rust_Code/004_rust_variable/main.rs
@@ -0,0 +1,19 @@
+/*
+
+let x = 12
+x + 5 = ?
+12 + 5 = 17
+
+*/
+
+fn main() {
+    // variable declaration
+    let number = 5000;
+    println!("{}", number);
+
+    let number2 = 3200;
+    println!("{}", number2);
+
+    let sum = number + number2;
+    println!("The sum of {} and {} is {}", number, number2, sum);
+}
