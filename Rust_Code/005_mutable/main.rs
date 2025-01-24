d11117fdc6a62a4056fe9eca2e3eacf07ed5eb3e
Abhishek (Desktop) <1171abhishek.com>
2025-01-24 16:19:25 +0530

    video 5 added

---
+++ Rust_Code/005_mutable/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // immuable variable (by default)
+    let a = 12;
+    println!("{}", a);
+    // a = 23;
+    // println!("{}", a);
+
+    // mutable variable
+    let mut age = 12;
+    println!("{}", age);
+    age = 20;
+    println!("{}", age);
+}
