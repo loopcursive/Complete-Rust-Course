9f8ddf367e8c4cff5f7dbc1b729b6e4f944a92b2
Abhishek (Desktop) <1171abhishek.com>
2025-01-25 13:25:21 +0530

    video 6 added

---
+++ Rust_Code/006_naming/main.rs
@@ -0,0 +1,21 @@
+fn main(){
+    let my_name_is_123 = 12;
+    println!("{}", my_name_is_123);
+
+    let _dfjhg = 12;
+    println!("{}", _dfjhg);
+
+    let my_friend_name_is = 21;
+    println!("{}", my_friend_name_is);
+
+    let a = 45;
+    println!("{}", a);
+
+    let A = 98;
+    println!("{}", A);
+
+    let let = 45;
+    // println!("{}", let);
+
+
+}
