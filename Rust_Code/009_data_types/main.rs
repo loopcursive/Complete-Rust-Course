d37c6546bd2db738bc0a1e01847e8f7259d81c91
Abhishek (Desktop) <1171abhishek.com>
2025-02-25 17:06:09 +0530

    data types code added

---
+++ Rust_Code/009_data_types/main.rs
@@ -0,0 +1,55 @@
+fn main(){
+    // Signed Integer
+    let a: i8 = -128;
+    println!("{}", a);
+
+    let b: i16 = 30000;
+    println!("{}", b);
+
+    let c : i32 = 1234455;
+    println!("{}", c);
+
+    // unsigned integer
+
+    let d: u8 = 1;
+    println!("{}", d);
+
+    let e : u32 = 123455;
+    println!("{}", e);
+
+    //  isize and usize
+    let f: isize = 9223372036854775806;
+    println!("{}", f);
+
+    let g : usize = 1233456;
+    println!("{}", g);
+
+    // f32
+    let h: f32 = 123.4551;
+    println!("{}", h);
+
+
+    // f64
+    let i: f64 = 123.455621545451;
+    println!("{}", i);
+
+
+    let j = 12.4545456;
+    println!("{}", j);
+
+
+    // Boolean
+
+    let yes: bool = true;
+    println!("{}", yes);
+
+
+    let no: bool = false;
+    println!("{}", no);
+
+
+    // Character
+    let z = '';
+    println!("{}", z);
+
+}
