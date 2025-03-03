aa740eada1bd005f6508a1f7131e05226771d39a
Abhishek (Desktop) <1171abhishek.com>
2025-03-03 17:03:04 +0530

    video 13 added

---
+++ Rust_Code/013_Asso_Prece/practise.rs
@@ -0,0 +1,25 @@
+/*  |__________________Practise Problems__________________|
+
+1.  What will be the output? ->  8 + 4 / 2;
+
+2.  Predict the output -> (10 - 3) * 2;
+
+3.  Fill in the missing parentheses to make the output 18. -> 6 + 4 * 3;
+
+4.  What will be the output? -> 20 / 5 * 2 - 3 + 1;
+
+5.  What will be the output? -> (30 / 5) + 2 * (6 - 4);
+
+6.  What will be the output? -> 10 - 2 + 4;
+
+7.  What will be the output? -> 16 / 4 * 2;
+
+8.  What will be the output? -> (50 / (5 + 5)) * (8 - 3) + 2 * (4 - 1);
+
+9.  What will be the output? -> (100 / (4 * 5)) + ((6 - 2) * 3) - (8 / 4) * 2;
+
+*/
+
+//  ()     - We can use () to modify the order of operations 
+//  BODMAS - We cannot use BODMAS in programming for evaluvating expressions instead 
+//           we use operator precedence and associativity.
