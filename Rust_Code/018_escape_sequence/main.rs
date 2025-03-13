3dd1753949adcd74d886a7c94635ef87c8a1cd60
Abhishek (Desktop) <1171abhishek.com>
2025-03-13 17:12:00 +0530

    video 18 code added

---
+++ Rust_Code/018_escape_sequence/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // \n for new line
+    println!("Hello\nWorld!");
+
+    // "\" for double quote
+    println!("Hello\"World!");
+
+    // \' for single quote
+    println!("Hello\'World!");
+
+    // \\ for backslash
+    println!("Hello\\World!");
+
+    // \t for tab
+    println!("Hello\tWorld!");
+
+    // \r for carriage return
+    println!("abcd efgh \r123456 pro");
+}
