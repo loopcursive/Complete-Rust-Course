6d6ac47cb0d50cdae2c2d7ccc5bb24a8df1d85ad
Abhishek (Desktop) <1171abhishek.com>
2025-03-19 17:14:10 +0530

    conditionals code added if else and else if

---
+++ Rust_Code/021_conditionals/mian.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // if else
+    let age = 10;
+
+    if age >= 18 {
+        println!("You can vote");
+    } else {
+        println!("You can't vote");
+    }
+
+    //  else if
+    let number = 0;
+    if number > 0 {
+        println!("Positive number");
+    } else if number < 0 {
+        println!("Negative number");
+    } else {
+        println!("Zero");
+    }
+
+    // if
+
+    let num = 12;
+    if num == 12 {
+        println!("This is equal to 12");
+    }
+
+    println!("print after if");
+}
